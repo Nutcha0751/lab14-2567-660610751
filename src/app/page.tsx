00e9429325a8da885f48589f58ef86ba54507c1f
Nutcha0751 <nutcha_kham@cmu.ac.th>
2024-09-06 01:09:50 +0700

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,55 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Text, Title, Space, Rating, Textarea, Button, Divider,Group, Pagination } from "@mantine/core";
+import { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState(0);
+
   return (
+    <div>
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
 
+      <Space h = "sm"/>
+      <Title order={4}>Your rating</Title>
+      <Rating defaultValue={0} color="orange" size="28.6px" style={{ gap: '1.4px' }}/>
+
+      <Textarea
+      label={<span style={{ fontSize: '13px', fontWeight: 600 }}>Your review</span>}
+      placeholder="Do you enjoy eating?"
+      rows={3}
+      mt="sm"
+      />
+
+      <Button mt = "9px" variant="filled" color="orange">Submit Review</Button>
+
+      <Group grow>
+        <Divider my="sm" />
+      </Group>
+
+      <Group justify="center" >
+        <Title order={4}>Elon Musk</Title>
+        <Rating value={5} readOnly/>
+      </Group>
+      <Text ta="center" c="dimmed" fz="15">Best pizza in this world. I give you X score.</Text>
+      <Group grow>
+        <Divider my="sm" />
+      </Group>
+
+      <Group justify="center" >
+        <Title order={4}>Mark Zuck</Title>
+        <Rating value={4} readOnly/>
+      </Group>
+      <Text ta="center" c="dimmed" fz="15">My favourite part is peporoni</Text>
+
+      <Group justify="center" my="sm">
+        <Pagination total={20} color="orange"/>
+      </Group>
+      
       <Text ta="center" my="sm">
         Copyright © 2024 Nutcha Khampoung 660610751
       </Text>
     </Container>
+    </div>
   );
 }
